73cb68f6fc579592c5fb47c7a892f1db8684108f
Tony Mullen <tony.mullen@gmail.com>
2019-11-14 16:20:37 -0800

    Starts chapter 3

--- BookProject/src/App.jsx
+++ BookProject/src/App.jsx
@@ -1,11 +1,22 @@
-const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-const helloContinents = Array.from(continents, c => `Hello ${c}`);
-const message = helloContinents.join(' ');
+class HelloWorld extends React.Component {
+    render(){
+        const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
+        const helloContinents = Array.from(continents, c => `Hello ${c}`);
+        const message = helloContinents.join(' ');
 
-const element = (
-    <div title="Outer div">
-        <h1 className="reddio">{message}</h1>
-    </div>
-);
+        return (
+            <div>
+                <div title="Outer div">
+                    <h1 className="reddio">{message}</h1>
+                </div>
+                <div>
+                    <h1 className="greenio">{message}</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+const element = <HelloWorld />;
 
 ReactDOM.render(element, document.getElementById('content'));
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Pro MERN Stack book notes
 
+## Chapter 3
+
+Notes:
+
 ## Chapter 2
 
-Notes: 
+Notes:
 
-* The description of the HTML on page 17 is cursory and assumes you know the basics of HTML, but listing 2-1 includes the full HTML necessary to get started. 
+* The description of the HTML on page 17 is cursory and assumes you know the basics of HTML, but listing 2-1 includes the full HTML necessary to get started.
 * Listing 2-1 has the div id as "contents". It should be "content" to go with the rest of the code described.
 * The author often starts talking about code and only later tells you where to put it. This seems to be a deliberate stylistic choice. Keep reading and it should become clear where the code goes (in this case on page 28).
