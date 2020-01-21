166d04751e07bccaf11da2d41143989d8e5fe133
Tony Mullen <tony.mullen@gmail.com>
2020-01-20 16:55:49 -0800

    Adds dotenv to ui

--- BookProject/ui/uiserver.js
+++ BookProject/ui/uiserver.js
@@ -1,8 +1,22 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(8000, function() {
-    console.log('UI started on port 8000')
+const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000';
+console.log(UI_API_ENDPOINT);
+
+const env = { UI_API_ENDPOINT };
+console.log(env)
+
+app.get('/env.js', function(req, res) {
+    res.send(`window.ENV = ${JSON.stringify(env)}`)
+})
+
+const port = process.env.UI_SERVER_PORT || 8000;
+
+app.listen(port, function() {
+    console.log(`UI started on port ${port}`)
 });
+
