2c5c31f627c07082f4cbf63af738776af8f4ed95
Tony Mullen <tony.mullen@gmail.com>
2020-01-23 15:19:32 -0800

    Adds graphql proxy functionality

--- api/server.js
+++ api/server.js
@@ -112,8 +112,10 @@ const server = new ApolloServer({
 });
 
 const app = express();
+const enableCors = (process.env.ENABLE_CORS || 'true') == 'true';
+console.log('CORS setting:', enableCors);
 
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 
 (async function() {
     try {
