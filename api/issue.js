d6e82d035a93558e4618014637f71f55a366d51e
Tony Mullen <tony.mullen@gmail.com>
2020-01-26 21:21:39 -0800

    Add search parameter for status

--- api/issue.js
+++ api/issue.js
@@ -1,10 +1,12 @@
 const { UserInputError } = require('apollo-server-express');
 const { getDb, getNextSequence } = require('./db.js');
 
-async function list() {
+async function list(_, { status }) {
   const db = getDb();
+  const filter = {};
+  if (status) filter.status = status;
   const issues = await db.collection('issues')
-    .find({}).toArray();
+    .find(filter).toArray();
   return issues;
 }
 
---
+++ ui/src/IssueEdit.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function IssueEdit({ match }) {
+  const { id } = match.params;
+  return (
+    <div>
+      <h2>{`This is a placeholder for editing issue ${id}`}</h2>
+    </div>
+  );
+}
