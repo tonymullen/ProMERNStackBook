81c25af1c136150e508b55e1e84f089cdfd27d81
Tony Mullen <tony.mullen@gmail.com>
2020-02-01 11:59:59 -0800

    Add drop-down menu and programmatic filter

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 /* eslint "react/prefer-stateless-function": "off" */
 
-export default class IssueFilter extends React.Component {
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+  }
+
+  onChangeStatus(e) {
+    const status = e.target.value;
+    const { history } = this.props;
+    history.push({
+      pahtname: '/issues',
+      search: status ? `?status=${status}` : '',
+    });
+  }
+
   render() {
     return (
       <div>
-        <Link to="/issues">All Issues</Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=New' }}>
-          New Issues
-        </Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>
-          Assigned Issues
-        </Link>
+        Status:
+        {' '}
+        <select onChange={this.onChangeStatus}>
+          <option value="">(All)</option>
+          <option value="New">New</option>
+          <option value="Assigned">Assigned</option>
+        </select>
       </div>
     );
   }
 }
+
+export default withRouter(IssueFilter);
